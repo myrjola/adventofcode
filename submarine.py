cb4932f81fa64eabbe0cde70a5271e14f306d662
Martin Yrjölä <martin.yrjola@gmail.com>
2021-12-01 20:52:23 +0200

    Second puzzle solved

--- submarine.py
+++ submarine.py
@@ -1,9 +1,17 @@
 with open("submarine.txt", "r") as file:
-    prev = float('inf')
-    larger_than_prev = 0
+    depths = []
     for line in file:
-        if float(line) > prev:
-            larger_than_prev += 1
-        prev = float(line)
-    print(larger_than_prev)
+        depths.append(float(line))
 
+    summed_windows = []
+    for start in range(len(depths)-2):
+        summed_windows.append(sum(depths[start:start+3]))
+
+    prev_sum = summed_windows[0]
+    larger_than_prev = 0
+    for summed_window in summed_windows[1:]:
+        if (summed_window > prev_sum):
+            larger_than_prev += 1
+        prev_sum = summed_window
+
+    print(larger_than_prev)
