566355f9295a37efcc9ca31eac9247a7af7329bb
Martin Yrjölä <martin.yrjola@gmail.com>
2021-12-05 09:13:34 +0200

    Solve day 5 part 2

--- 2021/05/hydrothermal.py
+++ 2021/05/hydrothermal.py
@@ -14,6 +14,7 @@ for (start, end), vector in zip(coordinates, vectors):
     if 0 not in vector:
         continue
 
+    # This logic was flawed but worked for part 1 😅
     length = int(np.linalg.norm(vector))
     unit = vector / length
 
@@ -26,5 +27,17 @@ for (start, end), vector in zip(coordinates, vectors):
 
 print("Part 1: ", len(np.where(board >= 2)[0]))
 
+board = np.zeros((1000,1000), dtype=np.int64)
 
+for (start, end), vector in zip(coordinates, vectors):
+    unit = np.nan_to_num(vector / abs(vector))
+    length = round(np.linalg.norm(vector) / np.linalg.norm(unit))
 
+    vent_cloud_points = [start + (unit * i) for i in range(length + 1)]
+
+    for x, y in vent_cloud_points:
+        board[int(y)][int(x)] += 1
+
+# print("\n".join(["".join(map(str,line)) for line in board]))
+
+print("Part 2: ", len(np.where(board >= 2)[0]))
