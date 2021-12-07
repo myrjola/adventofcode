daf0a6a77284a3234fc371a1f05195415b97b713
Martin Yrjölä <martin.yrjola@gmail.com>
2021-12-07 07:44:27 +0200

    Solve day 7 part 2

--- 2021/07/crabs.py
+++ 2021/07/crabs.py
@@ -1,11 +1,19 @@
-
 crabs = list(map(int, open("input.txt", 'r').readline().strip().split(',')))
 
 fuel_costs = [sum([abs(crab - position) for crab in crabs]) for position in range(max(crabs))]
+
+print('part 1: ', min(fuel_costs))
+
+def move(start, target):
+    dist = abs(start - target)
+    return sum(range(dist + 1))
+
+fuel_costs_part2 = [sum([move(crab, position) for crab in crabs]) for position in range(max(crabs))]
+
+print('part 2: ', min(fuel_costs_part2))
 
 # plot the fuel costs
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.plot(range(len(fuel_costs)), fuel_costs)
-
-print('part 1: ', min(fuel_costs))
+ax.plot(range(len(fuel_costs_part2)), fuel_costs_part2)
